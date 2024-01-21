658c8665c75652c0a52e75c72ab56e462b3097e4
unknown <andrirahmat6937@gmail.com>
2024-01-21 11:39:39 +0700

    update menu admin

--- app/Views/template/menus.php
+++ app/Views/template/menus.php
@@ -4,6 +4,9 @@
 
 <?= $this->section('menu'); ?>
 
+<li class="menu-header small text-uppercase">
+    <span class="menu-header-text">Master</span>
+</li>
 <li class="menu-item active open">
     <a href="javascript:void(0);" class="menu-link">
         <i class="menu-icon tf-icons bx bx-home-circle"></i>
@@ -12,39 +15,84 @@
     </a>
 </li>
 
-<!-- Layouts -->
+<!-- Barang -->
 <li class="menu-item">
     <a href="javascript:void(0);" class="menu-link">
-        <i class="menu-icon tf-icons bx bx-layout"></i>
-        <div data-i18n="Layouts">Layouts</div>
+        <i class="menu-icon tf-icons bx bxs-component"></i>
+        <div data-i18n="Layouts">Barang</div>
     </a>
 </li>
 
-<li class="menu-header small text-uppercase">
+<!-- kategori -->
+<li class="menu-item">
+    <a href="javascript:void(0);" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-list-ul"></i>
+        <div data-i18n="Layouts">Kategori</div>
+    </a>
+</li>
+
+<!-- member -->
+<li class="menu-item">
+    <a href="javascript:void(0);" class="menu-link">
+        <i class="menu-icon tf-icons bx bxs-user-detail"></i>
+        <div data-i18n="Layouts">Member</div>
+    </a>
+</li>
+
+<!-- Toko -->
+<li class="menu-item">
+    <a href="javascript:void(0);" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-store-alt"></i>
+        <div data-i18n="Layouts">Toko</div>
+    </a>
+</li>
+
+<!-- User -->
+<li class="menu-item">
+    <a href="javascript:void(0);" class="menu-link">
+        <i class="menu-icon tf-icons bx bxs-user-account"></i>
+        <div data-i18n="Layouts">User</div>
+    </a>
+</li>
+
+<!-- <li class="menu-header small text-uppercase">
     <span class="menu-header-text">Apps &amp; Pages</span>
+</li> -->
+<li class="menu-header small text-uppercase">
+    <span class="menu-header-text">Transaksi</span>
 </li>
 
+<!-- Transaksi -->
 <li class="menu-item">
     <a href="javascript:void(0);" class="menu-link">
-        <i class="menu-icon tf-icons bx bx-envelope"></i>
-        <div data-i18n="Layouts">Email</div>
+        <i class="menu-icon tf-icons bx bx-cart-add"></i>
+        <div data-i18n="Layouts">Sewa</div>
     </a>
 </li>
 
-<!-- Components -->
-<li class="menu-header small text-uppercase"><span class="menu-header-text">Components</span></li>
-<!-- Cards -->
+<!-- Laporan -->
+<li class="menu-header small text-uppercase"><span class="menu-header-text">Laporan</span></li>
+<!-- Laporan Barang -->
 <li class="menu-item">
     <a href="cards-basic.html" class="menu-link">
-        <i class="menu-icon tf-icons bx bx-collection"></i>
-        <div data-i18n="Basic">Cards</div>
+        <i class="menu-icon tf-icons bx bxs-book"></i>
+        <div data-i18n="Basic">Inventory</div>
     </a>
 </li>
 
+<!-- Laporan Sewa -->
 <li class="menu-item">
-    <a href="icons-boxicons.html" class="menu-link">
-        <i class="menu-icon tf-icons bx bx-crown"></i>
-        <div data-i18n="Boxicons">Boxicons</div>
+    <a href="cards-basic.html" class="menu-link">
+        <i class="menu-icon tf-icons bx bxs-cart-download"></i>
+        <div data-i18n="Basic">Sewa</div>
+    </a>
+</li>
+
+<!-- Laba Rugi -->
+<li class="menu-item">
+    <a href="cards-basic.html" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-money"></i>
+        <div data-i18n="Basic">Laba Rugi</div>
     </a>
 </li>
 
